67aa7b0fdba9f52d4fe25c3e0e7266bad3eecad1
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-08-06 14:03:38 +0100

    Ticket 2 completed

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,26 @@
-export function calculateScrabbleScore(word) {}
+export function calculateScrabbleScore(word) {
+
+  // TICKET 2
+  const letterScores = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+  };
+  // Green stage in TDD cycle
+  /*
+  if (word === "A") {
+    return 1;
+  }
+  */
+
+  // Refactor stage in TDD cycle
+  /*
+  return word === "A" ? 1 : 0;
+  return word === "B" ? 3 : 0;
+  */
+
+  // Final solution
+  // Return the matching score for the provided word from our letterScores object
+  return letterScores[word];
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,12 @@
+import { expect, test } from "vitest";
 import { calculateScrabbleScore } from "./scrabble-score";
+
+// Ticket 2
+// Write a new test that checks whether calculateScrabbleScore correctly calculates the score for the word A
+
+test("Calculate the score for the word A", function () {
+  expect(calculateScrabbleScore("A")).toBe(1);
+  expect(calculateScrabbleScore("B")).toBe(3);
+  expect(calculateScrabbleScore("C")).toBe(3);
+  expect(calculateScrabbleScore("D")).toBe(2);
+});
