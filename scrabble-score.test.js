d063ec9e8ecc08501e4b4eca7a2fd70ab0f1319e
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-08-06 14:19:14 +0100

    Implemented test.each. Ticket 2 step4

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -4,9 +4,18 @@ import { calculateScrabbleScore } from "./scrabble-score";
 // Ticket 2
 // Write a new test that checks whether calculateScrabbleScore correctly calculates the score for the word A
 
-test("Calculate the score for the word A", function () {
-  expect(calculateScrabbleScore("A")).toBe(1);
-  expect(calculateScrabbleScore("B")).toBe(3);
-  expect(calculateScrabbleScore("C")).toBe(3);
-  expect(calculateScrabbleScore("D")).toBe(2);
-});
+// test("Calculate the score for the word A", function () {
+//   expect(calculateScrabbleScore("A")).toBe(1);
+//   expect(calculateScrabbleScore("B")).toBe(3);
+//   expect(calculateScrabbleScore("C")).toBe(3);
+//   expect(calculateScrabbleScore("D")).toBe(2);
+// });
+
+test.each([
+  ["A", 1],
+  ["B", 3],
+  ["C", 3],
+  ["D", 2],
+])('calculateScrabbleScore(%s) -> %s', (letter, value) => {
+  expect (calculateScrabbleScore(letter)).toBe(value)
+})
